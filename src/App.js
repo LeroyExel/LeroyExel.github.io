85cd25cb2c6b1d7a32a757855ba79756b31121fe
Leroy <Yykgames1283@gmail.com>
2025-09-29 14:07:53 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import logo from "./assets/profile.jpg";
 import "./App.css";
-import { link } from "motion/react-client";
 
 function App() {
   const [projects, setProjects] = useState([]);
